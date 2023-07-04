2b4cbb8f030401ec7c7c1264af7fa60c652d8565
hamiduddin-aziz <98247095+hamiduddin-aziz@users.noreply.github.com>
2023-07-04 22:44:18 +0800

    Add files via upload

---
+++ lab/css/lab16/submission/Group_E/test.php
@@ -0,0 +1,18 @@
+<?php
+
+$favcol = "blue";
+
+switch($favcol){
+    case "red":
+        echo " yeye red";
+    case "blue":
+        echo "bluesssss";
+    case "green":
+        echo "mkh";
+    break;
+    default:
+    echo "idk";
+    
+}
+?>
+
