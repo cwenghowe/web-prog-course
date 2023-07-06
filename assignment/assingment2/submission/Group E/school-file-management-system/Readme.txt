cc26ec9f7f1f9785190e977d00dce83fa68506f9
hamiduddin-aziz <98247095+hamiduddin-aziz@users.noreply.github.com>
2023-07-06 16:03:45 +0800

    Add files via upload

---
+++ assignment/assingment2/submission/Group E/school-file-management-system/Readme.txt
@@ -0,0 +1,14 @@
+
+Group E
+
+
+1. Name: Noor Lailawati Abu Bakar, Matrix No: Sx222141ECJHF03, Github ID:  akma-laila
+
+
+2. Name: Azraei Hamiduddin bin Abdul, Matrix No: SX222146ECJHF03, Github ID: Hamiduddin-aziz
+
+
+admin side
+
+username: admin
+Password: admin
---
+++ assignment/assingment2/submission/Group E/school-file-management-system/admin/home.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<?php 
+	require 'validator.php';
+	require_once 'conn.php'
+?>
+<html lang = "en">
+	<head>
+		<title>School File Management System</title>
+		<meta charset = "utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+	</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<label class="navbar-brand">School File Management System</label>
+			<?php 
+				$query = mysqli_query($conn, "SELECT * FROM `user` WHERE `user_id` = '$_SESSION[user]'") or die(mysqli_error());
+				$fetch = mysqli_fetch_array($query);
+			?>
+			<ul class = "nav navbar-right">	
+				<li class = "dropdown">
+					<a class = "user dropdown-toggle" data-toggle = "dropdown" href = "#">
+						<span class = "glyphicon glyphicon-user"></span>
+						<?php 
+							echo $fetch['firstname']." ".$fetch['lastname'];
+						?>
+						<b class = "caret"></b>
+					</a>
+				<ul class = "dropdown-menu">
+					<li>
+						<a href = "logout.php"><i class = "glyphicon glyphicon-log-out"></i> Logout</a>
+					</li>
+				</ul>
+				</li>
+			</ul>
+		</div>
+	</nav>
+	<?php include 'sidebar.php'?>
+	<div id = "content">
+		<br /><br /><br />
+		<div class="alert alert-info"><h3>Mission</h3></div> 
+		<p>To develop a functional data-centric online system</p>
+		<div class="alert alert-info"><h3>Vision</h3></div> 
+		<p>Pass the course with fly colour mark</p>
+	</div>
+	</div>
+	<div id = "footer">
+		<label class = "footer-title">&copy; Copyright School File Management System <?php echo date("Y", strtotime("+8 HOURS"))?></label>
+	</div>
+<?php include 'script.php'?>	
+</body>
+</html>
